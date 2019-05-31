63cc57308c6e423ccb88d6d5058c6f2b5486be1f
Jacob McFaul <mcjakedude@gmail.com>
2019-05-31 08:55:03 -0700

    oop1 finished

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,32 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+# Vehicle is the base class of all of these subclasses
+class Vehicle:
+  def __init__(self):
+    #   GroundVehicle is a subclass of Vehicle
+    class GroundVehicle(Vehicle):
+      def __init__(self):
+        pass
+        #   Car is a subclass of Vehicle and GroundVehicle
+        class Car(GroundVehicle):
+          def __init__(self):
+            pass
+        #   Motorcycle is a subclass of Vehicle and GroundVehicle
+        class Motorcycle(GroundVehicle):
+          def __init__(self):
+            pass
+    #   FlightVehicle is a subclass of Vehicle
+    class FlightVehicle(Vehicle):
+      def __init__(self):
+        pass
+        #   Starship is a subclass of FlightVehicle and Vehicle
+        class Starship(FlightVehicle):
+          def __init__(self):
+            pass
+        #   Airplane is a subclass of FlightVehicle and Vehicle
+        class Airplane(FlightVehicle):
+          def __init__(self):
+            pass
